43233c2345ed9567d0466128bbab528f5b0d7910
dlsso <supertonesfan@hotmail.com>
2014-07-22 10:41:30 -0600

    Done

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
+// This needs to go through the server twice, once to get the page, one to get main.js
 app.get('/', function(req, res) {
-	res.send('<h1>Hello Boulder</h1>');
+	res.send('<h1>Hello Boulder</h1><script src="/main.js"></script>');
 });
 
 var server = app.listen(9909, function() {
